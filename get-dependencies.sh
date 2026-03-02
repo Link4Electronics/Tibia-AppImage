5f577d9b35ff9e0bf39ce33b8ea2f93cd3a3d864
Link4Electronics <link4electronics@gmail.com>
2026-03-02 01:31:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,6 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    curl-impersonate \
     glu    \
     python
 
@@ -16,15 +15,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-export DLAGENTS=("https::/usr/bin/curl-impersonate-chrome -fLC - --retry 3 --retry-delay 3 -o %o %u")
-make-aur-package tibia
+# Site blocks github
+PRE_BUILD_CMDS="sed -i 's|https://static.tibia.com/download/tibia.x64.tar.gz|https://api.rv.pkgforge.dev&|' ./PKGBUILD" make-aur-package tibia
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
