a89b073c2bcdfb1a6ec619c7b50d6c7cf914989d
Link4Electronics <link4electronics@gmail.com>
2026-03-02 03:09:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export STARTUPWMCLASS=com.cipsoft.tibia
 export DEPLOY_QT=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/tibia
+quick-sharun /usr/bin/tibia usr/lib/libxml2-legacy/lib/*
 
 # Additional changes can be done in between here
 
