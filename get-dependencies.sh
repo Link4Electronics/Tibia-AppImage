06c7e8cf441fb1bf6a49e458a0826a7d3d8e59b6
Link4Electronics <link4electronics@gmail.com>
2026-03-02 03:01:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-# Site blocks github
+# Site blocks github, added the mirror inside the PKGBUILD since had to patch with sed multiple lines and it wasn't working
 make-aur-package
 
 # If the application needs to be manually built that has to be done down here
