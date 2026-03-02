bfaa97a4e9b05a781f073284a42a3e0e2ddb1958
Link4Electronics <link4electronics@gmail.com>
2026-03-02 02:16:46 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,6 +16,6 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 # Site blocks github
-PRE_BUILD_CMDS="sed -i 's|https://static.tibia.com\"|https://api.rv.pkgforge.dev/https://static.tibia.com\")|; /agreement.php/d; /html2text/d; s/sha256sums=(.*/sha256sums=(\"SKIP\")/;' ./PKGBUILD" make-aur-package tibia
+PRE_BUILD_CMDS="sed -i 's|https://static.tibia.com\"|https://api.rv.pkgforge.dev/https://static.tibia.com\")|; /agreement.php/d; /html2text/d; s/sha256sums=(.*/sha256sums=(\"SKIP\")/ ; /\"SKIP\")/! { /SKIP/d }' ./PKGBUILD" make-aur-package tibia
 
 # If the application needs to be manually built that has to be done down here
