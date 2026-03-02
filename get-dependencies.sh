1a49669ad21894a3de6ffa33e9cedb964d1808f8
Link4Electronics <link4electronics@gmail.com>
2026-03-02 02:19:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,8 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    glu    \
+    gendesk \
+    glu     \
     python
 
 echo "Installing debloated packages..."
