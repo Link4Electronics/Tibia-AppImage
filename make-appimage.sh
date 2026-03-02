47c6ba824a20ced889d43f879406165391b49003
Link4Electronics <link4electronics@gmail.com>
2026-03-02 03:00:48 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/tibia.desktop
+export STARTUPWMCLASS=com.cipsoft.tibia
 export DEPLOY_QT=1
 
 # Deploy dependencies
