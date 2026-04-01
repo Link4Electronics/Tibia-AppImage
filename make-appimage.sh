75375ecdd98571e52cc6f179eeb2739ff658c59e
Link4Electronics <link4electronics@gmail.com>
2026-03-31 22:46:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DEPLOY_QT=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/Tibia
+quick-sharun ./AppDir/bin/Tibia ./AppDir/bin/client
 
 # Additional changes can be done in between here
 
