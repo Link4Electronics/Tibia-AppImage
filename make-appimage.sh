4bca1ac53a889f24422b8d22ce9769e837d74e51
Link4Electronics <link4electronics@gmail.com>
2026-03-31 22:18:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,7 @@ export DESKTOP=/usr/share/applications/tibia.desktop
 export DEPLOY_QT=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/Tibia
 
 # Additional changes can be done in between here
 
